8d515d13534cf5f00b22e679db3746507dc0c1a6
Marcin Gorniakowski <marcin@gorniakowski.pl>
2021-09-04 13:45:15 +0200

    Button click

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+const quoteContainer = document.getElementById('guote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
 let apiQuotes = [];
 
 function newQuote() {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    if (!quote.author) {
+        authorText.textContent = "Unknown";
+    } else {
+    authorText.textContent = quote.author;
+    }
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent= quote.text;    
 }
 
 
@@ -17,5 +34,13 @@ async function getQuotes() {
         console.log('error', error);
     }
 }
+
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 getQuotes();
